6a79c6bb59e59bf6698ba0c793bf95c1758b1138
whitewizardd <o.samuel@native.semicolon.africa>
2024-02-26 13:17:19 +0100

    updated

---
+++ contracts/Degen.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Degen is ERC20, Ownable {
+
+    enum RewardItems { 
+        PHOENIX, 
+        TIMEMACHINE, 
+        INVISIBLECLOAK,
+        MAGMAWAND
+    }
+
+    constructor() ERC20("Degen", "DWN") Ownable(msg.sender) {}
+
+
+    function calculateDecimals() private view returns(uint){
+        return 10 ** decimals();
+    }
+
+    function mintDegenToken(uint256 amount) public onlyOwner {
+        _mint(msg.sender, (amount * calculateDecimals()));
+    }
+
+    function mintDegenToken(address[] memory players, uint256 amount) public onlyOwner {
+        for (uint i = 0; i < players.length; i++) {
+            _mint(players[i], (amount * calculateDecimals()));
+        }
+    }
+
+    function transferDegenToken(address _to, uint256 _amount) external returns(bool){
+
+        require((_amount * calculateDecimals()) <= balanceOf(msg.sender), "Insufficient DegenWiz Token");
+
+        bool isSent = transfer(_to, (_amount * calculateDecimals()));
+
+        return isSent;
+    }
+
+    function burnDegenToken(uint256 _amount) external {
+        require((_amount * calculateDecimals()) <= balanceOf(msg.sender), "Insufficient DegenWiz Token");
+        
+        _burn(msg.sender, (_amount * calculateDecimals()));
+    }
+
+    function getDegenTokenToken() public view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function gainRewardItems(RewardItems item) external {
+        uint256 price = getPrice(item);
+        require(price <= balanceOf(msg.sender), "Insufficient DegenWiz Token");
+
+        _transfer(msg.sender, address(this), price);
+    }
+
+    function getPrice(RewardItems item) private view returns (uint256) {
+        if (item == RewardItems.PHOENIX) {
+            return 10 * calculateDecimals();
+        } else if (item == RewardItems.TIMEMACHINE) {
+            return 8 * calculateDecimals();
+        } else if (item == RewardItems.INVISIBLECLOAK) {
+            return 6 * calculateDecimals();
+        } else if (item == RewardItems.MAGMAWAND) {
+            return 4 * calculateDecimals();
+        } else {
+            return 0;
+        }
+    }
+
+    function withdrawFunds()external onlyOwner{
+        _transfer(address(this), msg.sender, balanceOf(address(this)));
+    }
+
+}
+
